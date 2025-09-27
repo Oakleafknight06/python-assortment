08b6bb0a426d913eee5c5e1ede2a3b2b5a2173c0
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2025-09-27 00:13:45 -0400

    Update passwordEntropyCalcualtor.py

--- passwordEntropyCalcualtor.py
+++ passwordEntropyCalcualtor.py
@@ -1,31 +1,43 @@
 import math
 
 # Explanation of the program
-print("Password complexity, or entropy, can only truly be calculated by evaluating the randomness of the password generation method, not by analyzing the password itself.")
+print("Password entropy, or randomness, can only truly be calculated by evaluating the randomness of the password generation method, not by analyzing the password itself.")
 
 # Alphabet selection
-print("The alphabet refers to the set of characters (or more generically, units) with which you assemble the password.")
+print("The alphabet refers to the set of characters or words with which you assemble the password.")
 # characters, or wordlist?
-is_wordlist = input("Are you using a wordlist (for passphrases)? (Y/N): ")
-if is_wordlist == "Y":
+is_wordlist = input("Are you using a wordlist (ie making a passphrases)? (y/N): ")
+if is_wordlist == ("Y" or "y"):
     wordlist_len = int(input("Enter the length of your wordlist: "))
     alphabet = wordlist_len
 else:
-    print("Select what you would like to include in your alphabet.")
+    print("\nOkay, starting alphabet selection process.\nSelect what you would like to include in your alphabet.")
     ab_len = 0
-    lowercase = input("Include lowercase letters? (Y/N): ")
-    uppercase = input("Include uppercase letters? (Y/N): ")
-    numbers = input("Include numbers 0-9? (Y/N): ")
-    special = input("Include these special characters?\n!@#$%^&*()_+-=`~,./;\'[]\\<>?:\"{}|\nThese are all the special characters available on a regular keyboard.\n (Y/N): ")
 
-    if lowercase == "Y":
+    lowercase = input("Include lowercase letters? (y/N): ")
+    if lowercase == ("Y" or "y"):
         ab_len += 26
-    if uppercase == "Y":
+    else:
+        print("Not including lowercase letters")
+
+    uppercase = input("Include uppercase letters? (y/N): ")
+    if uppercase == ("Y" or "y"):
         ab_len += 26
-    if numbers == "Y":
+    else:
+        print("Not including uppercase letters")
+
+    numbers = input("Include numbers 0-9? (y/N): ")
+    if numbers == ("Y" or "y"):
         ab_len += 10
-    if special == "Y":
+    else:
+        print("Not including numbers")
+
+    special = input("Include these special characters?\n!@#$%^&*()_+-=`~,./;\'[]\\<>?:\"{}|\nThese are all the special characters available on a regular keyboard.\n (y/N): ")
+    if special == ("Y" or "y"):
         ab_len += 32
+    else:
+        print("Not including special characters")
+
     print(f"Your alphabet has {ab_len} characters")
     alphabet = ab_len
 
