c3dacd4fe9ae6c7d14f690f7cc7f0fa222c5b553
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2025-10-15 12:51:16 -0400

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -1,38 +1,45 @@
 import math
+import string
 
-def calc_entropy(alphabet, selections):
-    combination = pow(alphabet, selections)
+def calc_entropy(alphabet_len, selections):
+    """ Takes an integer 'alphabet_len' and an integer 'selections', and returns the number of possible combinations, and the entropy"""
+    combination = pow(alphabet_len, selections)
     entropy = math.log2(combination)
-    return entropy
+    return combination, entropy
 
-def create_alphabet(lowercase = False, uppercase = False, numbers = False, special = False, custom = []):
-    # TODO: First four variables are boolean, last is a list and is optional. If arguments are not supplied, it is assumed false.
-    # TODO: Also return a list of the characters in the alphabet, rather than just the length
+def create_alphabet(lowercase = False, uppercase = False, numbers = False, special = False, custom = None):
+    """Boolean arguments: lowercase, uppercase, numbers, special. Also accepts a dictionary 'custom' for any other characters.
+       If arguments are not supplied, they are assumed false/empty.
+    """
 
     # Define variables
     ab_len = 0
     alphabet = set()
     special_charset = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "`", "~", ",", ".", "/", ";", "'", "[", "]" "\\", "<", ">", "?", ":", "\"", "{", "}", "|"}
 
+    # Processing
     if lowercase == True:
         ab_len += 26
+        alphabet = alphabet | set(string.ascii_lowercase)
 
     if uppercase == True:
         ab_len += 26
+        alphabet = alphabet | set(string.ascii_uppercase)
 
     if numbers == True:
         ab_len += 10
+        alphabet = alphabet | set(string.digits)
 
     if special == True:
         ab_len += 32
-        alphabet = alphabet.union(special_charset)
+        alphabet = alphabet | special_charset
 
-    if custom:
+    if custom: # TODO If custom is a set or list, otherwise error
         ab_len += len(custom)
+        alphabet = alphabet | set(custom)
+        # TODO Make sure the mutable object does not persist between function calls.
 
-    # return alphabet
-    # how to return two things?
-    return ab_len
+    return ab_len, alphabet
 
 def passphrase(wordlist, dice_list):
     print("unfinished")
