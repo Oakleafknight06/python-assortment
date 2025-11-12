48d507c777320249b7f44143e273a844a7cd7c56
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2025-11-12 18:00:20 -0500

    update entropy.py

--- entropy.py
+++ entropy.py
@@ -7,9 +7,8 @@ def calc_entropy(alphabet_len, selections):
     entropy = math.log2(combination)
     return combination, entropy
 
-def create_alphabet(lowercase = False, uppercase = False, numbers = False, special = False, custom = None):
-    """Boolean arguments: lowercase, uppercase, numbers, special. Also accepts a dictionary 'custom' for any other characters.
-       If arguments are not supplied, they are assumed false/empty. """
+def create_alphabet(lowercase = False, uppercase = False, numbers = False, special = False):
+    """Boolean arguments: lowercase, uppercase, numbers, special. If arguments are not supplied, they are assumed False. """
 
     # Define variables
     ab_len = 0
@@ -24,18 +23,17 @@ def create_alphabet(lowercase = False, uppercase = False, numbers = False, speci
         alphabet = alphabet | set(string.digits)
     if special == True:
         alphabet = alphabet | set(string.punctuation)
-    if custom: # TODO If custom is a set or list, otherwise error
-        alphabet = alphabet | set(custom)
-        # TODO Make sure the mutable object does not persist between function calls.
 
-    ab_len = len(alphabet)
-    return ab_len, alphabet
+    return alphabet
 
 def passphrase(wordlist, num_words, separation_char = "-", dice = False):
-    print("unfinished")
     user_list = []
-    for i in num_words:
-        # Get wordlist file
+    prog_list = []
+    with open(wordlist) as wordfile:
+    print(prog_list)
+
+    for i in range(num_words):
+        # Get wordlist file. Open file based on path provided by user? Or offer options/include only own file..
         if dice == True:
             # Ask the user for input from their dice
             print("oops, not implemented")
@@ -48,3 +46,5 @@ def passphrase(wordlist, num_words, separation_char = "-", dice = False):
     user_passphrase = separation_char.join(user_list)
     return user_passphrase
 
+if __name__ == "__main__":
+    passphrase("orchard-street-medium.txt", 4)
